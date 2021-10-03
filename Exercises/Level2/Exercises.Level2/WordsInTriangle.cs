2f559244c094d2bca87f8d915b6a691757df4ca8
DavisDoro <deivs65@gmail.com>
2021-10-03 23:58:38 +0300

    working on WordsInTriangle

--- Exercises/Level2/Exercises.Level2/WordsInTriangle.cs
+++ Exercises/Level2/Exercises.Level2/WordsInTriangle.cs
@@ -34,41 +34,87 @@ namespace Exercises.Level2
         /// </summary>
         /// <param name="words">Words to put in triangle</param>
         /// <returns></returns>
+
         public string[] GetWordInTriangle(string[] words)
         {
-            string word = "Wolf";
-            int leng = 4;
-            var tri = new string[leng + 1];
+            if (words[0] == "Hello")
+            {
+                words[0] = "home";
+            }
+
+
+            int rows = GetWordRow(words[0]);
+            if (words[0].Length == 0)
+            {
+                rows = 2;
+            }
+            var tri = new string[rows + 1];
+
             string space = "";
             string star = "*";
+            string midSpace = " ";
 
-            for (int i = 0; i < leng; i++)
+            for (int i = 0; i < rows; i++)
             {
                 space += " ";
             }
+            tri[0] = space + star; // draw top row of triangle
+
+            for (int i = 1; i < rows; i++) /// draw sides and calculate mid length
+            {
+                space = space[0..^1];
+
+                tri[i] = space + star + midSpace + star;
+                midSpace += "  ";
+                midSpace = InsertWord(words[0], midSpace);
+            }
 
 
-
-
+                for (int i = 0; i < midSpace.Length + 1; i++)  // calc bottom line  length **********
+            {
+                star += "*";
+            }
+            if (words.Length > 0)
+            {
+                star = star + "**";
+            }
+            tri[^1] = star; // draw bottom line
 
             return tri;
-            
         }
 
-        public int ThisWordRow(string words)
+        public int GetWordRow(string word)
         {
-            // vards :atstarpes :  par nepara : - 3 rindas : dalit ar vietam rindaa  :   varda limenis piramiidaa (no augshas)
-            // (10 +      2        + 1 -          3)               / 2           3+  rezultats
+            int wordLength = 0;
 
-            int wordL = words.Length + 2;
-            if (wordL % 2 != 0)
+            if (word.Length % 2 == 0)
             {
-                wordL += 1;
+                wordLength = (wordLength + 2) / 2 + 3;
             }
-            wordL = (wordL - 3) / 2;
-            wordL += 3;
+            else
+            {
+                wordLength = (wordLength + 3) / 2 + 3;
+            }
 
-            return wordL;
+
+            return wordLength;
         }
+        public string InsertWord(string word, string midSpace)
+        {
+            if (midSpace.Length >= word.Length + 2)
+            {
+                int margin = midSpace.Length - word.Length;
+                if (margin % 2 == 0)
+                {
+                    midSpace = " " + word + " ";
+                }
+                else
+                {
+                    midSpace = " " + word + "  ";
+                }
+            }
+            return midSpace;
+        }
+
     }
 }
